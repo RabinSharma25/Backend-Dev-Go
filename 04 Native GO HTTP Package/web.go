b1c9097d0b5c7cf641748c12ce4efa4486cafaa1
Rabin Sharma <cruzosharmas22545@gmail.com>
2023-09-06 20:31:09 +0530

    Learned to serve a single static file using native go http package

--- 04 Native GO HTTP Package/web.go
+++ 04 Native GO HTTP Package/web.go
@@ -1,22 +1,16 @@
+// Serving a single static html file
+
 package main
 
 import (
-	"fmt"
 	"net/http" // the http native package
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		fmt.Fprint(w, "<h1>Hello this is the root route</h1>")
-	case "/cat":
-		fmt.Fprint(w, "<h1>Hello this is the cat route</h1>")
-	case "/dog":
-		fmt.Fprint(w, "<h1>Hello this is the dog route</h1>")
-	}
+	http.ServeFile(w, r, "index.html")
 }
 
 func main() {
 	http.HandleFunc("/", handler)     //
-	http.ListenAndServe(":3000", nil) // this will start the server in the
+	http.ListenAndServe(":3000", nil) // this will start the server in the the port 3000
 }
