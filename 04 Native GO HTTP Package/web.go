0b07127f781258790b43a0db5f08c62c856f7b81
Rabin Sharma <cruzosharmas22545@gmail.com>
2023-09-06 20:54:46 +0530

    Learned how to serve a static folder using the http package in go

--- 04 Native GO HTTP Package/web.go
+++ 04 Native GO HTTP Package/web.go
@@ -1,4 +1,4 @@
-// Serving a single static html file
+// Serving files statically from a folder
 
 package main
 
@@ -6,11 +6,32 @@ import (
 	"net/http" // the http native package
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
+func handler(w http.ResponseWriter, r *http.Request) { // this function will help us serve a serve an entire static folder
+	p := "." + r.URL.Path
+	if p == "./" {
+		p = "./static/index.html" // here the folder we are serving is named "static"
+	}
+	http.ServeFile(w, r, p)
 }
 
 func main() {
-	http.HandleFunc("/", handler)     //
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":3000", nil) // this will start the server in the the port 3000
 }
+
+//////////////////////// The above functionality can be also achieved from the code block given below ////////////////////
+
+/*
+
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.ListenAndServe(":3000", nil)
+}
+
+*/
