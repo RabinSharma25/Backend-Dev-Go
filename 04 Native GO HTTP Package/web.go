e4dff07f0f42f1017d07a6caab4cb21078b71f16
Rabin Sharma <cruzosharmas22545@gmail.com>
2023-09-07 21:05:59 +0530

    Learned how to Set and Get a cookie in go

--- 04 Native GO HTTP Package/web.go
+++ 04 Native GO HTTP Package/web.go
@@ -2,25 +2,34 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Read the cookie named "myCookie"
+	cookie, err := r.Cookie("Apple")
+	if err != nil {
+		// Cookie not found
+		fmt.Fprintf(w, "Cookie not found")
+		return
+	}
+
+	// Cookie found, print its value
+	fmt.Fprintf(w, "Cookie Value: %s", cookie.Value)
+}
+
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	cookie := http.Cookie{
+		Name:  "Apple",
+		Value: "Its a fruit",
+	}
+	http.SetCookie(w, &cookie)
+	w.Write([]byte("Cookie set successfully!"))
+}
+
 func main() {
-	// make GET request
-	response, error := http.Get("https://reqres.in/api/products")
-	if error != nil {
-		fmt.Println(error)
-	}
+	http.HandleFunc("/get-cookie", handler) // In this route we can access the cookie
+	http.HandleFunc("/", setCookie)         // the root route sets up a cookie
 
-	// read response body
-	body, error := io.ReadAll(response.Body)
-	if error != nil {
-		fmt.Println(error)
-	}
-	// close response body
-	response.Body.Close()
-
-	// print response body
-	fmt.Println(string(body))
+	http.ListenAndServe(":8080", nil)
 }
