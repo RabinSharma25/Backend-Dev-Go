b13ef5c73b873ea56ccf85fe1526a6570309d7e6
Rabin Sharma <cruzosharmas22545@gmail.com>
2023-09-08 23:08:21 +0530

    Learned to serve static folder and also built a restful CRUD api using Gin and GORM and postgreSQL

--- 05 Gin Web Framework/02 Serve Static Folder/web.go
+++ 05 Gin Web Framework/02 Serve Static Folder/web.go
@@ -8,8 +8,8 @@ func main() {
 	// Create a Gin router instance
 	router := gin.Default()
 
-	// Define a route to serve the static HTML file
-	router.StaticFile("/static", "./static/index.html") // go to the /static route to acces the index.html page
+	// Serve the static files from the "static" directory
+	router.Static("/static", "./static")
 
 	// Start the server
 	router.Run(":8080")
---
+++ 05 Gin Web Framework/03 Restful API and GORM Package/rest-api.go
@@ -0,0 +1,156 @@
+// using this api we will be aable to perform CRUD operations in the postgre database from a web-page
+
+// package main
+
+// import (
+// 	"github.com/gin-gonic/gin"
+// )
+
+// func main() {
+// 	// Create a Gin router instance
+// 	router := gin.Default()
+
+// 	// Serve the static files from the "static" directory
+// 	router.Static("/crud", "./Rest-Api-CRUD")
+
+// 	// Start the server
+// 	router.Run(":8080")
+// }
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID   uint `gorm:"primaryKey"`
+	Name string
+	Age  int
+}
+
+func main() {
+
+	dsn := "host=localhost user=postgres password=rabin@123 dbname=apple port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Error connecting to database", err)
+	} else {
+		fmt.Println("Successfully connected to database", db)
+	}
+	// Create
+
+	// Migrate the schema, the below line of code basically creates an empty table
+
+	// db.AutoMigrate(&User{})
+
+	// Create a Gin router instance
+	router := gin.Default()
+	// router.Static("/", "./Rest-Api-CRUD")
+	// Serve the form.html file when accessing the root route
+	router.GET("/", func(c *gin.Context) {
+		c.File("./Rest-Api-CRUD/index.html")
+	})
+
+	router.GET("/create", func(c *gin.Context) {
+		c.File("./Rest-Api-CRUD/create.html")
+	})
+
+	router.GET("/insert", func(c *gin.Context) {
+		c.File("./Rest-Api-CRUD/insert.html")
+	})
+	router.GET("/read", func(c *gin.Context) {
+		c.File("./Rest-Api-CRUD/read.html")
+	})
+	router.GET("/update", func(c *gin.Context) {
+		c.File("./Rest-Api-CRUD/update.html")
+	})
+	router.GET("/delete", func(c *gin.Context) {
+		c.File("./Rest-Api-CRUD/delete.html")
+	})
+
+	// Define a route to handle POST requests
+	router.POST("/insert-form", func(c *gin.Context) {
+		// Read form data from the request
+		name := c.PostForm("name")
+		id := c.PostForm("id")
+		age := c.PostForm("age")
+
+		intAge, _ := strconv.Atoi(age)
+		intId, _ := strconv.Atoi(id)
+
+		user1 := User{ID: uint(intId), Name: name, Age: intAge}
+		db.Create(&user1)
+
+		// fmt.Println("Successfully inserted to database")
+		fmt.Println("Name:", name)
+		fmt.Println("Id:", id)
+		fmt.Println("Age:", age)
+
+	})
+
+	router.POST("/update-form", func(c *gin.Context) {
+		// Read form data from the request
+		name := c.PostForm("name")
+		id := c.PostForm("id")
+		// age := c.PostForm("age")
+
+		// intAge, _ := strconv.Atoi(age)
+		intId, _ := strconv.Atoi(id)
+
+		//update
+		db.Model(User{}).Where("ID= ?", intId).Updates(User{Name: name})
+
+		// fmt.Println("Successfully inserted to database")
+		fmt.Println("Name:", name)
+		fmt.Println("Id:", id)
+
+	})
+
+	router.POST("/read-form", func(c *gin.Context) {
+		// Read form data from the request
+		name := c.PostForm("name")
+		id := c.PostForm("id")
+		age := c.PostForm("age")
+
+		intAge, _ := strconv.Atoi(age)
+		intId, _ := strconv.Atoi(id)
+
+		user1 := User{ID: uint(intId), Name: name, Age: intAge}
+		db.Create(&user1)
+
+		// fmt.Println("Successfully inserted to database")
+		fmt.Println("Name:", name)
+		fmt.Println("Id:", id)
+		fmt.Println("Age:", age)
+
+	})
+
+	router.POST("/delete-form", func(c *gin.Context) {
+		// Read form data from the request
+		name := c.PostForm("name")
+		id := c.PostForm("id")
+		age := c.PostForm("age")
+
+		intAge, _ := strconv.Atoi(age)
+		intId, _ := strconv.Atoi(id)
+
+		user1 := User{ID: uint(intId), Name: name, Age: intAge}
+		db.Create(&user1)
+
+		// fmt.Println("Successfully inserted to database")
+		fmt.Println("Name:", name)
+		fmt.Println("Id:", id)
+		fmt.Println("Age:", age)
+
+	})
+
+	// Start the server
+	router.Run(":3000")
+}
